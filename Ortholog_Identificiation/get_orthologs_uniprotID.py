f5370c64dfa9c449830e9bcc2f8fa2acffb51986
jlhuns <joshua.l.hunsaker@gmail.com>
2025-01-30 11:29:49 -0700

    added loading print statements

--- Ortholog_Identificiation/get_orthologs_uniprotID.py
+++ Ortholog_Identificiation/get_orthologs_uniprotID.py
@@ -16,6 +16,7 @@ KEGG_API_BASE_URL = "http://rest.kegg.jp"
 PERSISTENCE_FILE = FILEPATH.PERSISTANCE_FILE_LOCATION + "/orthologs_uniprotID.csv"
 
 def get_genes(KOID: str) -> List[str]:
+    print("Getting KOID Genes")
     data = Kegg_API.find_genes(KOID)
     pattern = r"^\S+"
     genes = re.findall(pattern, data, flags=re.MULTILINE) 
@@ -23,12 +24,14 @@ def get_genes(KOID: str) -> List[str]:
     return genes
 
 def find_common_genes(genes, TARGET_ORGANISMS_FILEPATH):
+    print("Getting common Genes")
     organismsDf = pd.read_csv(TARGET_ORGANISMS_FILEPATH) 
     keggCodes = organismsDf['kegg_organism_code'].tolist()
     common_genes = [gene for gene in genes if any(gene.split(':')[0] == kegg_code for kegg_code in keggCodes)]
     return common_genes
 
 def find_uniprot_ids(genes: List[str]):
+    print("Finding Uniprot Ids for common genes")
     data = Kegg_API.convert_KeggID_to_UniprotID(genes)
     pattern = r'up:(\S+)'
     uniprot_ids = re.findall(pattern, data, flags=re.MULTILINE) 
@@ -38,4 +41,5 @@ def get_uniprot_ids(KOID, target_organism):
     genes = get_genes(KOID)
     common_genes = find_common_genes(genes, target_organism)
     uniprot_ids = find_uniprot_ids(common_genes)
+    print(f"For {KOID}, returning uniprot IDs : {uniprot_ids}")
     return uniprot_ids
