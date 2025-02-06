83e10f0a33ee98e3a964cd139d6038730541f590
kristinbillings <kris10d@byu.edu>
2025-02-05 23:54:45 -0700

    pretty average conservation scores file added to analysis

--- Analysis/average_conservation_scores.py
+++ Analysis/average_conservation_scores.py
@@ -1,8 +1,10 @@
 import file_paths as FILE_PATH
 import pandas as pd
+import os
 
-def average_conservation_score(file):
-    conservation_df = pd.read_csv(file)
+def average_conservation_score(target_organism):
+    target_organism_path = FILE_PATH.GET_ANALYSIS_RESULT_FILE(target_organism)
+    conservation_df = pd.read_csv(target_organism_path)
     conservation_df = conservation_df.drop_duplicates(subset = "MSA_Binding_Location")
     descriptionAndScore = {}
 
@@ -17,14 +19,22 @@ def average_conservation_score(file):
             else:
                 descriptionAndScore[row["Description"]][row["conservationScore"]] = 1
         
-    print(descriptionAndScore)  
+    rows = []
 
+    # Loop through dictionary to create rows
+    for substance, sub_data in descriptionAndScore.items():
+        for key, count in sub_data.items():
+            rows.append([substance, key, count])
 
+    # Create pandas DataFrame
+    df = pd.DataFrame(rows, columns=['Description', 'conservationScore', 'Count'])
+
+    df.to_csv(os.path.join(FILE_PATH.GET_ORGANISM_ANALYSIS_FOLDER_PATH(target_organism), "All_Average_Scores.csv"))
                 
 
 
 if __name__ == "__main__":
-    test_file = FILE_PATH.GET_ANALYSIS_RESULT_FILE("target_bacteria")
-    print(average_conservation_score(test_file))
+
+    average_conservation_score("target_bacteria")
 
     
