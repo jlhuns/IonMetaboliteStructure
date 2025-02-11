8d7fa1377a9b5fa9ab818d03462f867823616273
jlhuns <joshua.l.hunsaker@gmail.com>
2025-02-10 17:57:40 -0700

    conservation scores now come from Muscle ALN

--- MSA/analyze_active_sites.py
+++ MSA/analyze_active_sites.py
@@ -76,12 +76,65 @@ def get_conservation_score(analysisDF, KOID: str, target_organism: str):
             if(residue_index == seq_position):
                 return MSA_index, value
             
+    def remove_identifier_from_line(line):
+        # Split at the first space and remove the identifier
+        original_line = line
+        new_line = line.split(maxsplit=1)[1] if len(line.split()) > 1 else ''
+        # Count the number of characters removed
+        deleted_chars = len(original_line) - len(new_line)
+        return new_line, deleted_chars
+            
     MSA_RESULTS_FILE_PATH = FILE_PATH.GET_KOID_MSA_ANALYSIS_FILE_PATH(KOID, target_organism)
     alignment = AlignIO.read(MSA_RESULTS_FILE_PATH, "clustal")
 
+    
+
+    with open(MSA_RESULTS_FILE_PATH, 'r') as inF:
+        lines = inF.readlines()[3:]
+        group = []
+        delchars = 0
+        data = []
+        grouped_lines = ""
+        for index in range(len(lines)-1):
+            line = lines[index]
+            next_line = lines[index + 1]
+            if not line.startswith(" "):
+                line, delchars = remove_identifier_from_line(line)
+                if delchars == 1:
+                    data.append(grouped_lines)
+                    # print(grouped_lines)
+                    grouped_lines = ""
+                grouped_lines += line
+            else:
+                line = line[delchars:]
+                line = line.replace(" ", "-")
+                grouped_lines += line
+        last_line = lines[-1]
+        if last_line.startswith(" "):
+            last_line = last_line[delchars:]
+            last_line = last_line.replace(" ", "-")
+            grouped_lines += last_line
+        
+        data.append(grouped_lines)
+        
+        columns = {}  # This will store the final result
+        current_column_key = 0  # Start with the first column key
+
+        # Process each group in the data
+        for group in data:
+            data_lines = group.strip().split("\n")  # Split the group into lines
+            transposed = zip(*data_lines)  # Transpose the rows to columns
+            
+            # Add the transposed columns to the final columns dictionary
+            for col in transposed:
+                columns[current_column_key] = list(col)
+                current_column_key += 1  # Increment the column key
+
+
+    def get_conservation_column_score(MSA_Position):
+        return columns[MSA_Position][-1]
+
     for index, row in analysisDF.iterrows():
-
-
         seq_id = row["UniProtID"]
         seq_record = next((record for record in alignment if record.id == seq_id), None)
         binding_location = row["Binding_Location"]
@@ -99,7 +152,7 @@ def get_conservation_score(analysisDF, KOID: str, target_organism: str):
             for MSA_index in range(MSA_index_start, MSA_index_end + 1):
                 column = [record.seq[MSA_index] for record in alignment]
                 values += seq_record.seq[MSA_index]
-                conservation_score_data += (clustal_symbol(column))
+                conservation_score_data += get_conservation_column_score(MSA_index)
                 residue_counts.append(dict(Counter(column)))
 
             
@@ -115,8 +168,7 @@ def get_conservation_score(analysisDF, KOID: str, target_organism: str):
             # Get position and conservation score for this single position
             MSA_index, value = get_positions(binding_location_int, seq_record.seq)
             column = [record.seq[MSA_index] for record in alignment]
-            conservation_score = clustal_symbol(column)
-
+            conservation_score = get_conservation_column_score(MSA_index)
             residue_counts = dict(Counter(column))
             # Update the conservation score in the DataFrame
             analysisDF.at[index, 'conservationScore'] = conservation_score
@@ -149,4 +201,4 @@ def analyze_MSA(KOID: str, target_organism: str):
 
 
 if __name__ == "__main__":
-    analyze_MSA("K00031", "target_bacteria.csv")
+    analyze_MSA("K00134", "target_bacteria.csv")
